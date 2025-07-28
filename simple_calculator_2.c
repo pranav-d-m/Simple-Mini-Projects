3bbef5b60dd1cd32f7da8ee3e4fa7ed374ef2464
pranav-d-m <pranavdm1802@gmail.com>
2025-07-28 12:04:00 +0530

    Add files via upload
    🧮 A simple calculator built using C with basic UI enhancements like colored console output and box borders. Performs addition, subtraction, multiplication, and division using ternary operators. Built as a beginner-friendly mini project.

---
+++ simple_calculator_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<windows.h>
+void setColor(int color) 
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+int main()
+{
+    setColor(11); // Light cyan
+
+    int a,b;
+    char op;
+    float res;
+
+    printf("╔═══════════════════════════╗\n");
+    printf("║     SIMPLE CALCULATOR     ║\n");
+    printf("╚═══════════════════════════╝\n\n");
+    printf("Enter any two numbers:");
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    printf("Enter the operator '+','-','/','x' ");
+    scanf(" %c",&op);
+
+    if (op != '+' && op != '-' && op != 'x' && op != '/')
+    {
+        printf("Enter operator: ");
+        scanf("%c",&op);
+    }
+
+    res = (op == '+') ? (a+b) :
+          (op == '-') ? (a-b) :
+          (op == 'x') ? (a*b) :
+          (op == '/') ? ((b != 0) ? ((float)a / b) : 0.0) :
+           0.0;
+    
+    if (op == '/' && b == 0) 
+    {
+        printf("Error: Cannot divide by zero.\n");
+    } 
+    else
+    {
+        printf("Result = %.2f\n", res);
+    }
+    return 0;
+}
